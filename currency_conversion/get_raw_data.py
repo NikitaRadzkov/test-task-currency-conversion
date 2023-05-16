683b3923ebb2663fb31ba317f9cd45babfa264c6
Nikita Radkov <nikita.radzkou@gmail.com>
2023-05-16 14:56:48 +0300

    Create get_raw_data function

--- currency_conversion/get_raw_data.py
+++ currency_conversion/get_raw_data.py
@@ -3,12 +3,12 @@ import requests
 import xml.etree.ElementTree as ElementTree
 
 
-def get_exchange_rate(source: str, target: str = "EUR") -> pd.DataFrame:
-    url = f"https://sdw-wsrest.ecb.europa.eu/service/data/EXR/M.{source}.{target}.SP00.A?detail=dataonly"
+def get_raw_data(identifier: str) -> pd.DataFrame:
+    url = f"https://sdw-wsrest.ecb.europa.eu/service/data/BP6/{identifier}?detail=dataonly"
     response = requests.get(url)
 
     if response.status_code != 200:
-        raise Exception(f"Failed to fetch exchange rate data. Status code: {response.status_code}")
+        raise Exception(f"Failed to fetch data. Status code: {response.status_code}")
 
     root = ElementTree.fromstring(response.content)
 
@@ -22,10 +22,9 @@ def get_exchange_rate(source: str, target: str = "EUR") -> pd.DataFrame:
         data.append({"TIME_PERIOD": time_period, "OBS_VALUE": obs_value})
 
     df = pd.DataFrame(data)
-
     return df
 
 
-exchange_rate = get_exchange_rate("GBP")
+raw_data = get_raw_data("M.N.I8.W1.S1.S1.T.N.FA.F.F7.T.EUR._T.T.N")
 
-print("Exchange Rate", exchange_rate)
+print("Raw Data", raw_data)
